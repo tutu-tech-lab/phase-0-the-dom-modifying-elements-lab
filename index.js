bd276a989f00040dd1a03cef8608c6118634b3b5
Joy <joy.macharia@student.moringaschool.com>
2023-07-01 16:14:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector("main");
+main.remove(main)
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Joy wanjiru is the champion"
+ document.body.append(newHeader)
